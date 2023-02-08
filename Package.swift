cad80ce5d916ea43eb5b845cc9a4e1133daab816
Mathijs Kadijk <mkadijk@gmail.com>
2023-02-08 15:21:43 +0100

    Add rpath to use when within macOS app bundle

--- Package.swift
+++ Package.swift
@@ -9,6 +9,13 @@ let package = Package(
         .executableTarget(
             name: "concurrency-cli-sample",
             dependencies: [],
-            linkerSettings: [.unsafeFlags(["-Xlinker", "-rpath", "-Xlinker", "@executable_path"])]),
+            linkerSettings: [
+                // When using a macOS app bundle it is recommended to put the dylib in the Frameworks folder and the binary in the Helpers folder
+                // This helps to avoid hard to debug signing issues: https://developer.apple.com/documentation/bundleresources/placing_content_in_a_bundle?language=objc
+                .unsafeFlags(["-Xlinker", "-rpath", "-Xlinker", "@executable_path/../Frameworks"]),
+                
+                // In other cases it is often practical to put the dylib next to the executable
+                .unsafeFlags(["-Xlinker", "-rpath", "-Xlinker", "@executable_path"])
+            ]),
     ]
 )
